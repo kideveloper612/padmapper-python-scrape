25a501d5c5955cc5cf43e4b8e0bf136d5957779e
kideveloper612 <developer61510@gmail.com>
2020-05-20 15:04:12 +0800

    Minor change

--- scrape.py
+++ scrape.py
@@ -6,8 +6,8 @@ import re
 import urllib.request
 import csv
 import os
-
-csv_header = [['PRICE', 'STREET', 'CITY_STATE', 'ADDRESS', 'AGENT_NAME', 'AGENT_PHONE', 'DESCRIPTION', 'APARTMENT_AMENITIES', 'BUILDING_AMENITIES', 'BEDROOMS']]
+import random, string
+csv_header = [['ADDRESS', 'CITY', 'STATE', 'LOW RANGE RENT', 'HIGH RANGE RENT', 'BEDS COUNT', 'BATHS COUNT', 'SQUARE FOOTAGE', 'PROPERTY NAME', 'UNIT DESCRIPTION', 'APARTMENT AMENITIES', 'BUILDING AMENITIES', 'AGENT NAME', 'AGENT PHONE', 'IMAGE NAME', 'REFERRAL LINK']]
 
 
 def write_direct_csv(lines, filename):
@@ -17,7 +17,7 @@ def write_direct_csv(lines, filename):
     :param filename: file name for saving
     :return:
     """
-    with open('output/%s' % filename, 'a', encoding="utf-8", newline='') as csv_file:
+    with open(filename, 'a', encoding="utf-8", newline='') as csv_file:
         writer = csv.writer(csv_file, delimiter=',')
         writer.writerows(lines)
     csv_file.close()
@@ -30,11 +30,23 @@ def write_csv(lines, filename):
     :param filename: file name for saving
     :return:
     """
-    if not os.path.isdir('output'):
-        os.mkdir('output')
-    if not os.path.isfile('output/%s' % filename):
+    if not os.path.isfile(filename):
         write_direct_csv(lines=csv_header, filename=filename)
     write_direct_csv(lines=lines, filename=filename)
+
+
+def read_progress():
+    global file_name
+    if not os.path.isfile(file_name):
+        return []
+    file = open(file_name, 'r', encoding='utf-8')
+    rows = list(csv.reader(file))
+    file.close()
+    result = []
+    for r in rows:
+        if len(r) == 16 and 'https' in r[15]:
+            result.append(r[15])
+    return result
 
 
 def pin_request(min_lat, min_lng, xz_token=''):
@@ -67,32 +79,51 @@ def pin_request(min_lat, min_lng, xz_token=''):
     return res
 
 
-def apart_request(url):
+def apart_request(url, image_name):
     """
     Get all data for a url
     :param url: URL for scrapping one page
-    :return:
+    :return: Boolean variable if image
     """
     apart = requests.get(url=url)
     soup = BeautifulSoup(apart.content, 'html5lib')
     price_soup = soup.find(class_='FullDetail_price___O0l5')
+    city, state, min_price, max_price = '', '', '', ''
+    dup_check = []
     if price_soup:
-        price = soup.find(class_='FullDetail_price___O0l5').text.strip()
+        price = soup.find(class_='FullDetail_price___O0l5').text
+        price_split = price.split('-')
+        if len(price_split) == 2:
+            min_price = price_split[0].strip()
+            max_price = price_split[1].strip()
+        elif len(price_split) == 1:
+            max_price, min_price = price_split[0].strip(), price_split[0].strip()
     else:
-        price = ''
+        min_price, max_price = 'Unknown', 'Unknown'
     street_soup = soup.find(class_='FullDetail_street__zq-XK')
     if street_soup:
         street_soup_parent = street_soup.parent
         street = street_soup.text.replace('\xa0', ' ').replace('\n', ' ').strip()
         street_soup.decompose()
-        city_state = street_soup_parent.text.replace('\xa0', ' ').replace('\n', ' ').strip()
+        property_name = street_soup_parent.text.replace('\xa0', ' ').replace('\n', ' ').strip()
     else:
-        street, city_state = '', ''
+        street, property_name = '', ''
     address_soup = soup.find(class_='SummaryTable_header__2gj_9', text=re.compile('Address'))
     if address_soup:
-        address = address_soup.next_sibling.text.replace('\xa0', ' ').replace('\n', ' ').strip()
+        address_c_s = address_soup.next_sibling.text.replace('\xa0', ' ').replace('\n', ' ').split(',')
+        if len(address_c_s) == 3:
+            address = address_c_s[0].strip()
+            city = address_c_s[1].strip()
+            state = address_c_s[2].strip()
+        elif len(address_c_s) == 2:
+            address = address_c_s[0].strip()
+            state = address_c_s[1].strip()
+        elif len(address_c_s) == 1:
+            address = address_c_s[0].strip()
+        else:
+            address, city, state = '', '', ''
     else:
-        address = ''
+        address, city, state = '', '', ''
     apartment_amenities, building_amenities = [], []
     gutters = soup.findAll(attrs={'class': 'row p-no-gutter undefined'})
     for gutter in gutters:
@@ -119,53 +150,89 @@ def apart_request(url):
         if len(agent_gutter) == 2:
             agent_name = agent_gutter[0].text.replace('\xa0', ' ').replace('\n', ' ').strip()
             agent_phone = agent_gutter[1].text.replace('\xa0', ' ').replace('\n', ' ').strip()
+        else:
+            agent_name, agent_phone = '', ''
+    else:
+        agent_name, agent_phone = '', ''
     preloaded_state = soup.find('script', text=re.compile("window.__PRELOADED_STATE__"))\
         .text.replace('window.__PRELOADED_STATE__', '').replace('=', '').replace('\xa0', ' ').replace('\n', ' ').strip()[:-1]
     preloaded = "{" + re.search('"entity":{(.*)"favoritesView":', preloaded_state).group(1)[:-1]
-    one_line = [price, street, city_state, address, agent_name, agent_phone, description, apartment_amenities, building_amenities]
     bedrooms = soup.find_all(class_='Floorplan_floorplansContainer__2Rtwg')
-    for bedroom in bedrooms:
-        count_bedroom = bedroom.find(class_='Floorplan_title__179XB').text.replace('\xa0', ' ').replace('\n', ' ').strip()
-        available_count = bedroom.find(class_='Floorplan_availabilityCount__RvEqU').text.replace('\xa0', ' ').replace('\n', ' ').strip()
-        bedroom_price_soup = bedroom.find(class_='Floorplan_priceRange__x-BQo')
-        if bedroom_price_soup:
-            bedroom_price = bedroom.find(class_='Floorplan_priceRange__x-BQo').text.replace('\xa0', ' ').replace('\n', ' ').strip()
+    if bedrooms:
+        for bedroom in bedrooms:
+            if bedroom.find(class_='Floorplan_title__179XB'):
+                count_bedroom = bedroom.find(class_='Floorplan_title__179XB').text.replace('\xa0', ' ').replace('\n', ' ').strip()
+            else:
+                count_bedroom = ''
+            if bedroom.find(class_='Floorplan_availabilityCount__RvEqU'):
+                available_count = bedroom.find(class_='Floorplan_availabilityCount__RvEqU').text.replace('\xa0', ' ').replace('\n', ' ').strip()
+            else:
+                available_count = ''
+            bedroom_price_soup = bedroom.find(class_='Floorplan_priceRange__x-BQo')
+            if bedroom_price_soup:
+                bedroom_price = bedroom.find(class_='Floorplan_priceRange__x-BQo').text.replace('\xa0', ' ').replace('\n', ' ').strip()
+            else:
+                bedroom_price = ''
+            if 'floorplan_listings' in json.loads(preloaded):
+                floor_plans = json.loads(preloaded)['floorplan_listings']
+                print(floor_plans)
+                for floor_plan in floor_plans:
+                    if 'bedrooms' in floor_plan and str(floor_plan['bedrooms']) in count_bedroom:
+                        if 'title' in floor_plan and floor_plan['title'] is not None:
+                            title =floor_plan['title'].replace('\xa0', ' ').replace('\n', ' ').strip()
+                        else:
+                            title = ''
+                        if 'square_feet' in floor_plan and floor_plan['square_feet'] is not None:
+                            square_feet = str(floor_plan['square_feet']).replace('\xa0', ' ').replace('\n', ' ').strip()
+                            if square_feet == '0':
+                                square_feet = ''
+                        else:
+                            square_feet = ''
+                        if 'bathrooms' in floor_plan and floor_plan['bathrooms'] is not None:
+                            bathroom_count = str(floor_plan['bathrooms']).replace('\xa0', ' ').replace('\n', ' ').strip() + ' Bathrooms'
+                        else:
+                            bathroom_count = ''
+                        amenity_tags_list = []
+                        if 'amenity_tags' in floor_plan and floor_plan['amenity_tags'] is not None:
+                            amenity_tags = floor_plan['amenity_tags']
+                            for ament in amenity_tags:
+                                amenity_tags_list.append(ament.replace('\xa0', ' ').replace('\n', ' '))
+                        min_price = str(floor_plan['min_price']).strip()
+                        max_price = str(floor_plan['max_price']).strip()
+                        if min_price == '0':
+                            min_price = max_price
+                        elif max_price == '0':
+                            max_price = min_price
+                        if min_price == '0' and max_price == '0':
+                            continue
+                        min_price = '$' + min_price
+                        max_price = '$' + max_price
+                        one_line = [address, city, state, min_price, max_price, count_bedroom, bathroom_count, square_feet, property_name, description, apartment_amenities, building_amenities, agent_name, agent_phone, image_name, url]
+                        if one_line not in dup_check:
+                            dup_check.append(one_line)
+                            print(one_line)
+                            write_csv(lines=[one_line], filename=file_name)
+    elif soup.find(attrs={'class': 'Description_feature__39cQ0 Description_standalone__1VmC2'}):
+        bedroom_card_soup = soup.find(class_='SummaryTable_header__2gj_9', text=re.compile('Bedrooms'))
+        if bedroom_card_soup:
+            bedroom_card_count = bedroom_card_soup.next_sibling.text.replace('\xa0', ' ').replace('\n', ' ')
         else:
-            bedroom_price = ''
-        bedroom_box = [count_bedroom, available_count, bedroom_price]
-        if 'floorplan_listings' in json.loads(preloaded):
-            floor_plans = json.loads(preloaded)['floorplan_listings']
-            for floor_plan in floor_plans:
-                if 'bedrooms' in floor_plan and str(floor_plan['bedrooms']) in count_bedroom:
-                    if 'title' in floor_plan and floor_plan['title'] is not None:
-                        title =floor_plan['title'].replace('\xa0', ' ').replace('\n', ' ').strip()
-                    else:
-                        title = ''
-                    if 'square_feet' in floor_plan and floor_plan['square_feet'] is not None:
-                        square_feet = str(floor_plan['square_feet']).replace('\xa0', ' ').replace('\n', ' ').strip()
-                    else:
-                        square_feet = ''
-                    if 'bathrooms' in floor_plan and floor_plan['bathrooms'] is not None:
-                        bathroom_count = str(floor_plan['bathrooms']).replace('\xa0', ' ').replace('\n', ' ').strip()
-                    else:
-                        bathroom_count = ''
-                    amenity_tags_list = []
-                    if 'amenity_tags' in floor_plan and floor_plan['amenity_tags'] is not None:
-                        amenity_tags = floor_plan['amenity_tags']
-                        for ament in amenity_tags:
-                            amenity_tags_list.append(ament.replace('\xa0', ' ').replace('\n', ' '))
-                    min_price = str(floor_plan['min_price']).strip()
-                    max_price = str(floor_plan['max_price']).strip()
-                    if min_price == '0':
-                        avaiable_price = max_price
-                    elif max_price == '0':
-                        avaiable_price = min_price
-                    else:
-                        avaiable_price = min_price + '~' + max_price
-                    bedroom_box.append([title, square_feet, bathroom_count, amenity_tags_list, avaiable_price])
-        one_line.append(bedroom_box)
-    print(one_line)
-    write_csv(lines=[one_line], filename=file_name)
+            bedroom_card_count = ''
+        bathroom_card_soup = soup.find(class_='SummaryTable_header__2gj_9', text=re.compile('Bathrooms'))
+        if bathroom_card_soup:
+            bathroom_card_count = bathroom_card_soup.next_sibling.text.replace('\xa0', ' ').replace('\n', ' ')
+        else:
+            bathroom_card_count = ''
+        square_card_soup = soup.find(class_='SummaryTable_header__2gj_9', text=re.compile('Square'))
+        if square_card_soup:
+            square_card_count = square_card_soup.next_sibling.text.replace('\xa0', ' ').replace('\n', ' ').replace('—', '')
+        else:
+            square_card_count = ''
+        one_line = [address, city, state, min_price, max_price, bedroom_card_count, bathroom_card_count, square_card_count, property_name, description, apartment_amenities, building_amenities, agent_name, agent_phone, image_name, url]
+        if one_line not in dup_check:
+            dup_check.append(one_line)
+            print(one_line)
+            write_csv(lines=[one_line], filename=file_name)
 
 
 def loop_apartments(data):
@@ -176,18 +243,18 @@ def loop_apartments(data):
     """
     base_url = 'https://www.padmapper.com/apartments/long-beach-ca/'
     for leaf in data:
+        image_name = 'None'
         if 'image_ids' in leaf and 'building_name' in leaf:
             image_name = leaf['building_name']
-            if leaf['image_ids'] is not None:
+            if leaf['image_ids'] is not None and len(leaf['image_ids']) > 0:
                 image_id = leaf['image_ids'][0]
                 if image_name is not None:
                     if '.jpg' in image_name:
                         image_name = image_name.replace(' ', '_').replace('/', '__')
                     else:
                         image_name = image_name.replace(' ', '_').replace('/', '__').replace('.', '') + '.jpg'
-                    download_image(image_id=image_id, name=image_name)
                 else:
-                    continue
+                    image_name = '%s.jpg' % ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(10))
         if 'pb_id' in leaf and leaf['pb_id'] is not None:
             url = '{}b-p{}'.format(base_url, leaf['pb_id'])
         elif 'pl_id' in leaf and leaf['pl_id'] is not None:
@@ -197,7 +264,11 @@ def loop_apartments(data):
         else:
             continue
         print(url)
-        apart_request(url=url)
+        if url in progress_urls:
+            print('Done already!')
+            continue
+        apart_request(url=url, image_name=image_name)
+        download_image(image_id=image_id, name=image_name)
 
 
 def download_image(image_id, name):
@@ -211,7 +282,10 @@ def download_image(image_id, name):
     print(image_url)
     if not os.path.isdir(image_directory):
         os.mkdir(image_directory)
-    urllib.request.urlretrieve(image_url, '{}/{}'.format(image_directory, name))
+    try:
+        urllib.request.urlretrieve(image_url, '{}/{}'.format(image_directory, name))
+    except Exception as e:
+        print(e)
 
 
 if __name__ == '__main__':
@@ -226,6 +300,7 @@ if __name__ == '__main__':
     image_directory = 'Image'
     file_name = 'result.csv'
     pin_url = 'https://www.padmapper.com/api/t/1/pins'
+    progress_urls = read_progress()
 
     for lat in np.arange(lat_bottom, lat_ceil, delta):
         for lng in np.arange(lng_bottom, lng_ceil, delta):
